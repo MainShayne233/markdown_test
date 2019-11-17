14cdd157c5f15800a819986873bfcd80fdf4e1d0
MainShayne233 <shaynetremblay@gmail.com>
2019-11-17 16:05:01 -0500

    Fix elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule MarkdownTest.MixProject do
     [
       app: :markdown_test,
       version: "0.1.0",
-      elixir: "~> 1.10-dev",
+      elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       test_coverage: [tool: ExCoveralls],
