4af0912a8f6bc7ace26980baf764290f432a9249
MainShayne233 <shaynetremblay@gmail.com>
2019-11-17 16:11:13 -0500

    Add hex package info

--- mix.exs
+++ mix.exs
@@ -8,6 +8,8 @@ defmodule MarkdownTest.MixProject do
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      description: description(),
+      package: package(),
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [
         coveralls: :test,
@@ -18,6 +20,16 @@ defmodule MarkdownTest.MixProject do
     ]
   end
 
+  defp description, do: "Test the Elixir code in your markdown files."
+
+  defp package do
+    [
+      name: "markdown_test",
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/MainShayne233/markdown_test"}
+    ]
+  end
+
   def application do
     []
   end
@@ -25,7 +37,8 @@ defmodule MarkdownTest.MixProject do
   defp deps do
     [
       {:mix_test_watch, "~> 0.9.0", only: :dev, runtime: false},
-      {:excoveralls, "~> 0.12.0", only: [:dev, :test], runtime: false}
+      {:excoveralls, "~> 0.12.0", only: [:dev, :test], runtime: false},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 end
